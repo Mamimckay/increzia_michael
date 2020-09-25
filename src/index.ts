5f7f1f6e01f07e86cd73928971d7f6364f0b4275
Mamimckay <71871892+Mamimckay@users.noreply.github.com>
2020-09-25 11:56:07 +0300

    NumberFinalVersion

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// EXERCICE 1
+
 class NumberFormat {
   constructor(number1: [], number2: [], number3: []) {
     this.number1 = number1;
@@ -20,3 +22,28 @@ class NumberFormat {
 let number1 = new NumberFormat([0, 1, 0], [1, 1, 0], [0, 0, 1]);
 number1.showArrayNumber()
 
+// EXERCICE 2
+
+class SommeEntier {
+  constructor(number: []) {
+    this.number = number;
+  }
+
+  positifEntier() {
+    let nombreTotal = this.number.map((nbre) => {
+      if (nbre > 0) {
+        console.log(`sommeEntier(2,tArray) = ${nbre}`);
+      }
+    })
+  }
+
+  arrayRange(){
+    let nombreRange = this.number.reverse();
+    console.log(nombreRange);
+  }
+}
+
+let sommeNumber = new SommeEntier([0, 3, -5, 12, -18, -20]);
+
+sommeNumber.positifEntier();
+sommeNumber.arrayRange();
