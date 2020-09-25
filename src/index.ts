383c64c6bd7e3d2a3f24dc229f43274d4f2659ee
Mamimckay <71871892+Mamimckay@users.noreply.github.com>
2020-09-25 11:10:40 +0300

    NumberFormat

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-const text: string = 'hello!!!'
-console.log(text);
+class NumberFormat {
+  constructor(number1: [], number2: [], number3: []) {
+    this.number1 = number1;
+    this.number2 = number2;
+    this.number3 = number3;
+  }
+
+  showArrayNumber() {
+    console.log(
+      `[
+        [${this.number1}],
+        [${this.number2}],
+        [${this.number3}],
+      ]
+      `
+    );
+  }
+}
+
+let number1 = new NumberFormat([0, 1, 0], [1, 1, 0], [0, 0, 1]);
+number1.showArrayNumber()
+
